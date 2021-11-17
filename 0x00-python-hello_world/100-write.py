a70e66430e610e2327dca76e4bd682b8a7a59e68
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-11-17 13:08:15 +0200

    fix write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
 import sys
 from sys import stderr
-stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 sys.exit(1)
