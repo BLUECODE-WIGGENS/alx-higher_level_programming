ed164a7c857ce3e32c8a3395447e01cc5ef6c0d6
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-11-17 14:19:35 +0200

    fixing

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
-    if(chr(c) >= chr(97)) and (chr(c) <= chr(123)):
+    if(ord(c) >= (97)) and (ord(c) <= (123)):
         return True
     else:
         return False
