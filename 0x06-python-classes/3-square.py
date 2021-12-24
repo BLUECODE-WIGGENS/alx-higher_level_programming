514c608b6d0504f30072807094d076d5eb0141a0
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-12-24 09:38:51 +0200

    square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""define square class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """
+        Only integer on input otherwise raises an
+        exception
+
+        size: type(int)
+        size: >= 0 otherwise raises exception
+        """
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """
+        method to return the current square area
+        """
+        return self.__size * self.__size
