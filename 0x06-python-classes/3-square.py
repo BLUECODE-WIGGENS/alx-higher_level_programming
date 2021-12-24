6d01e9dcff249287b9b674c1ed2015299a0c29f2
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-12-24 09:40:59 +0200

    fixed

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -18,6 +18,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        
     def area(self):
         """
         method to return the current square area
