e119ec2fb43cd622b8c2194f0b278efcaf0cac70
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-12-27 13:51:35 +0200

    nqueens

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+
+user_input = argv[1]
+
+if not isinstance(user_input, int):
+    print('N must be a number')
+    exit(1)
+if user_input <= 4:
+    print("N must be at least 4")
+    exit(1)
