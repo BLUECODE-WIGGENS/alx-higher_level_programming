455c568dd1b672a2c3e0d0d5c860203b29b45db9
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-12-27 11:52:40 +0200

    rectangle class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""define Rectangle module"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height =  height
+
+    @property
+    def width(self):
+        """return the instance menthod itself"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        sets the width method
+        Args:
+           value: integer
+        Raise:
+            TypeError: if not int raise an exception
+            ValueError: if int is < 0 raise an exception     
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        sets the height method
+
+        args:
+           value: integer
+        Raise:
+            TypeError: if not int raise exception
+            Valueerror: if int is < 0 raise exception
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            ValueError("height must be >= 0")
+
+        self.__height = value
