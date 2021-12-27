965f6b273568cdbc5968bb8da1b27ce62623d352
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-12-27 13:56:26 +0200

    nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -11,6 +11,7 @@ user_input = argv[1]
 if not isinstance(user_input, int):
     print('N must be a number')
     exit(1)
-if user_input <= 4:
+
+if user_input < 4:
     print("N must be at least 4")
     exit(1)
