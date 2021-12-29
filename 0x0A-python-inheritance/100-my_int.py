7861041dcae97f66cf94c1f2f450804feab131bc
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-12-29 13:57:21 +0200

    bug fixed

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """define MyInt module"""
 
+
 class MyInt(int):
     """MyInt class"""
 
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,17 +1,18 @@
 #!/use/bin/python3
 """add_attribute module"""
 
+
 def add_attrbute(object, attr, size):
-     """
-     Function that adds a new attribute to an object if it’s possible:
-     Args:
-        obj: the object add a new attribute.
-        attribute: attribute to add.
-        value: value to set the attribute.
-      Returns:
-        None: nothing
-     """
-     if hasattr(object, "__dict__"):
-         setattr(object, attr, size)
-         return
-     raise TypeError("can't add new attribute")
+    """
+    Function that adds a new attribute to an object if it’s possible:
+    Args:
+       obj: the object add a new attribute.
+       attr: attribute to add.
+       size: value to set the attribute.
+    Returns:
+       None: nothing
+    """
+    if hasattr(object, "__dict__"):
+        setattr(object, attr, size)
+        return
+    raise TypeError("can't add new attribute")
