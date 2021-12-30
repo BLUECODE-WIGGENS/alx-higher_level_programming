905e152b7e7774b766f51e8eca3e758caddfeb27
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2021-12-30 13:13:40 +0200

    handling files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""read_file module"""
+"""defins read_file module"""
 
 
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints it to stdout
-    Args:
-      filename: the name of the file to read from
-    """
+    """reads a text file (UTF8) and prints it to stdout"""
     with open("{}".format(filename), "r") as filename:
-      print(filename.read(), end="")
+        print(filename.read(), end="")
