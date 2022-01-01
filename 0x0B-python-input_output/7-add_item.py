1a5666d10857cd33acc889f6ec1d67aaeb38285b
BLUECODE-WIGGENS <samuelmcnores1@gmail.com>
2022-01-01 15:35:43 +0200

    file i/o

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""
-a script that adds all arguments to a Python list,
+"""Module with a script that adds all arguments to a Python list,
 and then save them to a file
 save_to_json_file from 5-save_to_json_file.py
 load_from_json_file from 6-load_from_json_file.py
 """
+import json
 from sys import argv
+"""imprting argv from sys"""
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -14,9 +15,11 @@ filename = "add_item.json"
 try:
     arg_list = load_from_json_file(filename)
 except Exception:
+    """raise FileaNotFoundError exception"""
     arg_list = []
 
 for element in argv[1:]:
+    """add a file to arg_list"""
     arg_list.append(element)
 
 save_to_json_file(arg_list, filename)
